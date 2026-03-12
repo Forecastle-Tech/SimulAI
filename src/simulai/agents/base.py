ae23ecc0cbc3b5feda32898ce8c1cad03a765908
Forecastle-Tech <info@forecastletech.com>
2026-03-12 11:35:29 -0700

    Create base.py

---
+++ src/simulai/agents/base.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+class Agent(ABC):
+    def __init__(self, name: str, x: int, y: int):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    @abstractmethod
+    def tick(self, world):
+        ...
